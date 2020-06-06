22b517feea619fd58e517f915f0e7f696ab83c98
dotrider <56666461+dotrider@users.noreply.github.com>
2020-06-05 17:41:35 -0700

    sockets connection set/server

--- server/index.js
+++ server/index.js
@@ -3,8 +3,19 @@ const express = require('express'),
     http = require('http'),
     PORT_NUM = process.env.PORT_NUM || 4040,
     app = express(),
-    server = http.createServer(app);
+    server = http.createServer(app),
     io = sockets(server);
 
+
+    io.on('conneection', (socket) => {
+        console.log('a user connected!!!!!');
+        socket.on('disconnect', () => {
+            console.log('connection disconnected');
+        });
+    });
+
+    app.get('/', (req, res) => {
+        res.send('send')
+    });
 
 server.listen(PORT_NUM, () => console.log(`spinning hardcore on port ${PORT_NUM}`));
